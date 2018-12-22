ab8c462f6e24a4b6eebe13d1bbb80f636543be0f
Kumar21 <perfectks21@gmail.com>
2018-12-23 02:00:13 +0530

    sadsa

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 module.exports.log2=callName;
 function callName(name){
-    console.log("hi",name)}
+   // console.log("hi",name)
+}
 
 const path = require('path');
 var pathObj = path.parse(__filename);
 
 const os = require('os')
-console.log(os.totalmem());
+//console.log(os.totalmem())
+
+const express = require('express');
+const app = express();
+app.get('/',(req,res)=>{
+    res.write('hey man');
+    res.end();
+});
+app.get('/course/:year/:month',(req,res)=>{
+    res.send(req.params);
+    res.end();
+})
+app.get('/api',(req,res)=>{
+    res.send(req.query);
+})
+const port= process.env.port || 3001
+app.listen(port,()=>console.log(`listening to the port ${port}..`))
