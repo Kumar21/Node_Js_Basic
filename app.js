da6077580ac31b86cd13ad338590f217f3fe77bc
Kumar21 <perfectks21@gmail.com>
2018-12-24 00:56:08 +0530

    get

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const os = require('os')
 
 const express = require('express');
 const app = express();
+const courses =[
+    {id:1,title:'course1'},
+    {id:2,title:'course2'},
+    {id:3,title:'course3'}
+]
 app.get('/',(req,res)=>{
     res.write('hey man');
     res.end();
@@ -19,8 +24,10 @@ app.get('/course/:year/:month',(req,res)=>{
     res.send(req.params);
     res.end();
 })
-app.get('/api',(req,res)=>{
-    res.send(req.query);
+app.get('/course/:id',(req,res)=>{
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send("404 course not found");
+    return res.send(course);
 })
 const port= process.env.port || 3001
 app.listen(port,()=>console.log(`listening to the port ${port}..`))
