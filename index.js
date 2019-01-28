743ef1b58fb4bd3517319e599478dd093dcf9508
Kumar21 <perfectks21@gmail.com>
2019-01-28 19:50:58 +0530

    ads

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const express = require('express');
 const genresRouter = require('./routes/genres')
 const courses = require('./routes/courses');
 const customer = require('./routes/customers');
-//const WorkingUser = require('./workingDir/users');
+const WorkingUser = require('./workingDir/users');
 const checkuser = require('./models/user');
+const auth = require('./models/auth')
 const app = express();
 
 //app.use('/api/genres',genresRouter);
 //app.use('/api/courses',courses);
 //app.use('/api/customers',customer);
 //app.use('/api/wuser',WorkingUser)
-app.use('/api/checkuser',checkuser);
+//app.use(log3);
+//app.use('/api/checkuser',checkuser);
+app.use('/api/auth',auth);
 app.use(express.json());
 app.use(express.static('./'));
- app.use(log3);
 
 app.set('view engine','pug');
 app.set('views','./views'); // default
--- models/auth.js
+++ models/auth.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
-const { User, validate } = require('./uservalidate');
+const { User } = require('./uservalidate');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -13,35 +13,27 @@ router.get('/', async (req, res) => {
   const user = await User.find().sort('name');
   res.send(user);
 });
+
 router.post('/', async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-
+  
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.');
-
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
-  /*   user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-    }); */
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
-
   await user.save();
-  // res.send(await User.find().sort('name'));
-  //res.send({id:user.id,name:user.name,email:user.email});
-  res.send(_.pick(user, ['_id', 'name', 'email']));
+    res.send(_.pick(user, ['_id', 'name', 'email']));
   console.log("success");
 });
 
-router.delete('/', async (req, res) => {
-  const name = await User.findOne({ name: req.body.name });
-  if (!name) return res.status(404).send('Name does not exist');
-  await User.deleteOne({ name: req.body.name })
-  res.send(await User.find().sort('name'));
-})
+function validate(user){
+    const Schema ={
+        password:Joi.string().min(3).required(),
+        email:Joi.string().min(3).required()
+    }
+   return Joi.validate(user,Schema);
+}
+
 module.exports = router;
-/* const port = process.env.PORT || 3005;
-router.listen(port, () => console.log(`Listening on port ${port}...`)); */
